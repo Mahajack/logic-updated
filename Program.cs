e95dba81668c32ecef661f7272c32d85a414bd6b
Mahajack <59824635+Mahajack@users.noreply.github.com>
2020-04-06 18:15:33 +0530

    BinarySort

--- Program.cs
+++ Program.cs
@@ -1,47 +1,38 @@
 ﻿using System;
 
-class InsertionSort
+class BinarySort
 {
-    // Function to sort array 
-    // using insertion sort 
-    void sort(int[] arr)
-    {
-        int input = arr.Length;
-        for (int input_array = 1; input_array < input; ++input_array)
-        {
-            int key = arr[input_array];
-            int j = input_array - 1;
 
-            // Move elements of arr[0..i-1], 
-            // that are greater than key, 
-            // to one position ahead of 
-            // their current position 
-            while (j >= 0 && arr[j] > key)
-            {
-                arr[j + 1] = arr[j];
-                j = j - 1;
-            }
-            arr[j + 1] = key;
-        }
-    }
-
-   
-    static void printArray(int[] arr)
-    {
-        int input = arr.Length;
-        for (int input_array = 0; input_array < input; ++input_array)
-            Console.Write(arr[input_array] + " ");
-
-        Console.Write("\n");
-    }
-
-    //main method
     public static void Main()
     {
-        int[] arr = { 12, 11, 13, 5, 6 };
-        InsertionSort obj = new InsertionSort();
-        obj.sort(arr);
-        printArray(arr);
+        int[] arr = {37, 23, 0, 17, 12, 72, 31, 
+							46, 100, 88, 54 };
+
+        sort(arr);
+
+        for (int input = 0; input < arr.Length; input++)
+            Console.Write(arr[input] + " ");
+    }
+
+    public static void sort(int[] array)
+    {
+        for (int input = 1; input < array.Length; input++)
+        {
+            int key = array[input];
+
+            // Find location to insert using 
+            // binary search 
+            int insert = Math.Abs(Array.BinarySearch(
+                            array, 0, input, key) + 1);
+
+            // Shifting array to one location right 
+            System.Array.Copy(array, insert, array,
+                                        insert + 1, input - insert);
+
+            // Placing element at its correct 
+            // location 
+            array[insert] = key;
+        }
     }
 }
 
